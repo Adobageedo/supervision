0528af1a5a98ccd5f2cc0d8b3e81a41c6ba5a936
Adobageedo <100688637+Adobageedo@users.noreply.github.com>
2025-12-01 18:07:29 +0100

    adding firebase

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   firebase: {
-    apiKey: 'YOUR_FIREBASE_API_KEY',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+    apiKey: '3DBzl4mJoKG9oB678OkeXyDQBspxMo5wtwjZKWUbw58',
+    authDomain: 'supervision-61462.firebaseapp.com',
+    projectId: 'supervision-61462',
+    storageBucket: 'supervision-61462.appspot.com',
+    messagingSenderId: '117661975139402070322',
+    appId: '1:117661975139402070322:web:06c2c2c2c2c2c2c2c2c2c2'
   }
 };
